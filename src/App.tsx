02b35fd5c59d92448abf10f6ec8aae6575086aab
Pham Van Khanh <khanhphamvan1910@gmail.com>
2022-06-23 14:55:49 +0700

    commit update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./styles/app.scss";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import TicketManage from "./components/ticket-manage/TicketManage";
+import TicketChange from "./components/ticket-change/TicketChange";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="wrapper">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/manage-ticket" element={<TicketManage />} />
+          <Route path="/change-ticket" element={<TicketChange />} />
+        </Routes>
+      </div>
     </div>
   );
 }
